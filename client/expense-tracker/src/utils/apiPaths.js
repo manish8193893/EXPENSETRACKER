cfd23256bb0ed229421801e0499c866a519f1982
Manish Kumar <manish8193892@gmail.com>
2025-07-11 16:31:50 +0530

    Update apiPaths.js

--- client/expense-tracker/src/utils/apiPaths.js
+++ client/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_SITE_URL || 'http://localhost:8000/';
+export const BASE_URL = import.meta.env.VITE_SITE_URL;
 
 // utils/apiPaths.js
 export const API_PATHS = {
